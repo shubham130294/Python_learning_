711044bf7392f64fb667848374211375e9de92c7
SHUBHAM SHARMA <58881296+shubham130294@users.noreply.github.com>
2020-05-10 02:34:13 +0530

    Data Types in Python

---
+++ 1.0 Data_types.py
@@ -0,0 +1,7 @@
+# Data Tyoes in Python :
+# None , Numeric , List, Tuple,Sets,Dictationary,String ,Range
+
+#None is equivalent to Null ,whenevr any variable is declared but not initialized .
+
+#Numeric : int /float/complex/bool
+
